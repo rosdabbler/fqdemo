319e6f7acaa7188c0ae8e39072a89496997cea80
Kent James <kent@caspia.com>
2021-12-27 11:37:24 -0800

    Try running rosdoc2

--- .github/actions/rosdoc2/main.sh
+++ .github/actions/rosdoc2/main.sh
@@ -7,3 +7,15 @@ git clone $INPUT_ROSDOC2_REPO rosdoc2
 git -C rosdoc2 checkout $INPUT_ROSDOC2_BRANCH
 
 python -m pip install ./rosdoc2
+
+echo "rosdoc2 installed, running"
+
+cd $GITHUB_WORKSPACE
+
+echo "workspace ls\n $(ls)"
+
+mkdir -p /results/build
+mkdir -p /results/cr
+mkdir -p /results/output
+
+rosdoc2 build --debug -d /results/build -c /results/cr -o /results/output -p ./
