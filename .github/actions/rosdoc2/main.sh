c3b489b62100f7aa6cd3da2d35e40208ccd4fdd3
Kent James <kent@caspia.com>
2021-12-27 11:30:12 -0800

    Checkout and install rosdoc2 in action

--- .github/actions/rosdoc2/main.sh
+++ .github/actions/rosdoc2/main.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "This is main.sh for the rosdoc2 action"
-env
+
+git clone $INPUT_ROSDOC2_REPO rosdoc2
+
+git -C rosdoc2 checkout $INPUT_ROSDOC2_BRANCH
+
+python -m pip install ./rosdoc2
