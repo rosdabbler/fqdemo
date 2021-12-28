2d734f3bfd21aeb22d03ca9f02b348b22bdc5edb
Kent James <kent@caspia.com>
2021-12-28 12:06:19 -0800

    1) use /tmp for rosdoc2 2) try pull of doc as suggested in actions output

--- .github/actions/rosdoc2/main.sh
+++ .github/actions/rosdoc2/main.sh
@@ -7,9 +7,9 @@ BASE_URL="https://rosdabbler.github.io/fqdemo"
 echo "This is main.sh for the rosdoc2 action"
 echo "rosdoc2 repo is $INPUT_ROSDOC2_REPO"
 
-git clone $INPUT_ROSDOC2_REPO rosdoc2
-git -C rosdoc2 checkout $INPUT_ROSDOC2_BRANCH
-python -m pip install ./rosdoc2
+git clone $INPUT_ROSDOC2_REPO /tmp/rosdoc2
+git -C /tmp/rosdoc2 checkout $INPUT_ROSDOC2_BRANCH
+python -m pip install /tmp/rosdoc2
 
 echo "rosdoc2 installed, running"
 
