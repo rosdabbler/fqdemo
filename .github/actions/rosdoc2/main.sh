d4a29c2591c38454f9c6c508d7ed2ada7de18607
Kent James <kent@caspia.com>
2021-12-28 11:42:16 -0800

    Update actions using docs branch and directory

--- .github/actions/rosdoc2/main.sh
+++ .github/actions/rosdoc2/main.sh
@@ -2,26 +2,19 @@
 
 set -x
 
-echo "This is main.sh for the rosdoc2 action"
+BASE_URL="https://rosdabbler.github.io/fqdemo"
 
+echo "This is main.sh for the rosdoc2 action"
 echo "rosdoc2 repo is $INPUT_ROSDOC2_REPO"
 
 git clone $INPUT_ROSDOC2_REPO rosdoc2
-
 git -C rosdoc2 checkout $INPUT_ROSDOC2_BRANCH
-
 python -m pip install ./rosdoc2
 
 echo "rosdoc2 installed, running"
 
 cd $GITHUB_WORKSPACE
 
-echo "workspace ls\n $(ls)"
-
-mkdir -p /results/build
-mkdir -p /results/cr
-mkdir -p /results/output
-
-rosdoc2 build --debug -d /results/build -c /results/cr -o /results/output -p ./fqdemo_msgs
-rosdoc2 build --debug -d /results/build -c /results/cr -o /results/output -p ./fqdemo_msgs
-rosdoc2 build --debug -d /results/build -c /results/cr -o /results/output -p ./
+rosdoc2 build --debug -o docs -u $BASE_URL -p ./fqdemo_msgs
+rosdoc2 build --debug -o docs -u $BASE_URL -p ./fqdemo_nodes
+rosdoc2 build --debug -o docs -u $BASE_URL -p ./
