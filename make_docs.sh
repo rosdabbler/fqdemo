e3bf6b91dc29e8c328a45b6b2b8f50e95ca1758c
R. Kent James <kent@caspia.com>
2022-06-09 12:48:31 -0700

    Support latest rosdoc2 changes

--- make_docs.sh
+++ make_docs.sh
@@ -3,6 +3,7 @@
 # This script will build the documentation locally for this collection
 
 URL="file://$(pwd)/docs_output"
-rosdoc2 build --debug -u $URL -p ./fqdemo_msgs
-rosdoc2 build --debug -u $URL -p ./fqdemo_nodes
-rosdoc2 build --debug -u $URL -p ./fqdemo
+#rosdoc2 build --debug -u $URL -p ./fqdemo_msgs
+#rosdoc2 build --debug -u $URL -p ./fqdemo_nodes
+#rosdoc2 build --debug -u $URL -p ./fqdemo
+rosdoc2 build --debug -u $URL -p ./
